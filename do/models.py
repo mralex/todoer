992fd02b72b17c28796168ad3491bf487fa3e1ab
Alex Roberts <bse@FauxPas.local>
2008-04-01 20:09:31 -0700

    Initial import of todo project.

---
+++ do/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+import datetime
+
+class Todo(models.Model):
+	todo = models.CharField(max_length = 200)
+	done = models.BooleanField()
+	due = models.DateTimeField('Date Due')
+	added = models.DateTimeField('Date Added')
